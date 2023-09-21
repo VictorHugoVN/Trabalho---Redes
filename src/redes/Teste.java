5a133580f2f2fc39cf79ad421a19df79ea7940fc
Victor Hugo <victorneivadef8@gmail.com>
2023-09-21 17:19:42 -0300

    Teste de manipulação de string (Classe Teste)

--- src/redes/Teste.java
+++ src/redes/Teste.java
@@ -4,4 +4,18 @@ public class Teste {
 
     // CLASSE PARA TESTES
 
+    public static void main(String[] args){
+
+
+        // Teste de manipulação de string
+
+        String palavra = "#Login     /#"; // Exemplo de comando emitido pelo cliente
+        String nova = palavra.replace(" ", ""); // Onde tiver espaços em branco será substituído por nada
+        nova = palavra.substring(palavra.indexOf("#") + 1, palavra.indexOf("/#")); // retira '#' e '/#'
+        System.out.println(nova);
+        nova = nova.trim(); // Retira possíveis espaços em branco antes ou depois da string desejada
+        System.out.println(nova.length()); // Tamanho da string
+
+    }
+
 }
