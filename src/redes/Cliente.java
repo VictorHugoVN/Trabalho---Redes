d3d11486c695a20fb50a5ffe45e60c80550c67f2
Victor Hugo <victorneivadef8@gmail.com>
2023-09-20 21:17:14 -0300

    Isso é um teste

--- src/redes/Cliente.java
+++ src/redes/Cliente.java
@@ -1,7 +1,9 @@
 package redes;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
  
 class Cliente {
     public static void main(String args[]) throws Exception {
